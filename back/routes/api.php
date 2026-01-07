545fdfef9c45b132694ff212efed9f6ea771b78a
Alexis BONNET <Alexis.Bonnet1@etu.univ-grenoble-alpes.fr>
2026-01-07 11:13:25 +0100

    (feat)[API] : create API routes

--- back/routes/api.php
+++ back/routes/api.php
@@ -1,10 +1,27 @@
 <?php
 
-use App\Http\Controllers\Api\TestController;
+use App\Http\Controllers\Api\GameController;
+use App\Http\Controllers\Api\PlayerController;
+use App\Http\Controllers\Api\GameFlowController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('api.key')
+    ->prefix('v1')
     ->name('api.')
     ->group(function () {
-        Route::apiResource('test', TestController::class);
-});
+
+        // Création d'une partie
+        Route::post('/games', [GameController::class, 'store']);
+
+        // Infos lobby
+        Route::get('/games/{code}', [GameController::class, 'show']);
+
+        // Rejoindre une partie
+        Route::post('/games/{code}/players', [PlayerController::class, 'store']);
+
+        // Actions de jeu
+        Route::post('/games/{code}/start', [GameFlowController::class, 'start']);
+
+        // Récupération du rôle (joueur connecté)
+        Route::get('/me/role', [PlayerController::class, 'role']);
+    });
