d7a80f186e79f8c16d7db5982530b88221ab2560
Devannblr <devann.billereau@gmail.com>
2026-01-09 15:52:54 +0100

    (feat)[Enigme2] Mise en forme de l'enigme 2

--- front/src/app/(enigmes)/enigme-2/page.jsx
+++ front/src/app/(enigmes)/enigme-2/page.jsx
@@ -1,8 +1,11 @@
 "use client";
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useRouter } from "next/navigation";
 import QRScanner from "@/components/organisms/QRScanner/QRScanner";
 import BaseModal from "@/components/molecules/Modals/BaseModal";
+import TypewriterTerminal from "@/components/molecules/TypewriterTerminal/TypewriterTerminal";
+import Link from "next/link";
+import {RiDoorLockBoxLine} from "react-icons/ri";
 
 export default function Enigme2Page() {
     const router = useRouter();
@@ -12,8 +15,21 @@ export default function Enigme2Page() {
     const [error, setError] = useState(false);
 
     return (
-        <main className="min-h-screen bg-[var(--color-dark)] flex flex-col items-center p-6">
+        <main className="h-full flex flex-col md:max-w-md mx-auto">
+            <section className="flex flex-col h-full">
+                {/* Zone de texte avec scroll */}
+                <article className="text-(--color-turquoise) py-4 px-4 font-mono text-xs overflow-y-auto flex-1 max-h-[15vh] border-2 border-(--color-turquoise) flex justify-around items-center">
+                    <div className="border-2 size-16"></div>
+                    <div className="border-2 size-16"></div>
+                    <div className="border-2 size-16"></div>
+                    <div className="border-2 size-16"></div>
+                </article>
 
+                {/* Zone des applications - toujours visible */}
+                <article className="flex flex-col gap-6 justify-center items-center py-8 text-white flex-1 overflow-hidden border-2">
+
+                </article>
+            </section>
         </main>
     );
 }
