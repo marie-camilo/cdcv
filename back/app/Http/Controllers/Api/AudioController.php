90802e1347c9bea7966ac3903d3df7507d30de01
Devannblr <devann.billereau@gmail.com>
2026-01-13 20:27:54 +0100

    (fix)[AudioController] Modification du fichier audioControlleur

--- back/app/Http/Controllers/Api/AudioController.php
+++ back/app/Http/Controllers/Api/AudioController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Game; // Import du modèle Game
+use App\Models\Game;
 use Illuminate\Support\Facades\Storage;
 
 class AudioController extends Controller
