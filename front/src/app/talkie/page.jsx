82d36de0cea222866cc8b7f0d4bdab7741f03cb4
Alexis BONNET <alexis.bonnet92140@gmail.com>
2026-01-13 16:24:54 +0100

    (fix)[FRONT] :  remove broken file

--- front/src/app/talkie/page.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import TalkieWalkie from '@/components/organisms/TalkieWalkie/TalkieWalkie';
-export default function TestTalkiePage() {
-    return (
-        <div>
-            <TalkieWalkie roomCode="test-room-123" />
-        </div>
-    );
-}
