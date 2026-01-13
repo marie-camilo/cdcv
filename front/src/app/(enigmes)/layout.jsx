88cbe22e4972e834ccf51acafd4226d0e97e7867
Alexis BONNET <alexis.bonnet92140@gmail.com>
2026-01-13 13:37:05 +0100

    (fix)[FRONT] : fix labyrinth

--- front/src/app/(enigmes)/layout.jsx
+++ front/src/app/(enigmes)/layout.jsx
@@ -4,14 +4,8 @@ import GameOverModal from "@/components/molecules/GameOverModal";
 
 export default function EnigmeLayout({ children }) {
     return (
-        <TimerProvider>
-            <Navbar/>
-            <div className="bg-dark text-light-green h-full flex flex-col overflow-hidden">
-                <div className="pb-4 flex-1 overflow-auto">
-                    {children}
-                </div>
-                <GameOverModal />
-            </div>
-        </TimerProvider>
+        <div className="pb-4 flex-1 overflow-auto">
+            {children}
+        </div>
     );
 }
